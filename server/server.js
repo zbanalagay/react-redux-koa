2777c820218a869354ab74b04b5234040323fce3
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-02 00:47:47 -0800

    [feat] add react hot module reloading and error catching

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
+
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config');
+const compiler = webpack(webpackConfig);
+
 const koa = require('koa');
 const serve = require('koa-static');
 const historyApiFallback = require('koa-connect-history-api-fallback');
-require('dotenv').config();
 
 const app = koa();
+
+app.use(require('koa-webpack-dev-middleware')(compiler, {
+  quiet: true,
+  noInfo: true,
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  publicPath: webpackConfig.output.publicPath
+}));
+app.use(function* (next) {
+  yield require('webpack-hot-middleware')(compiler).bind(null, this.req, this.res);
+  yield next;
+});
 
 app.use(historyApiFallback());
 app.use(serve(`${__dirname}/../app/dist`));
