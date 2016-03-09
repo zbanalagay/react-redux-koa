d5f2f3ffae9e880f54c097695c1d8c752e9d6321
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-08 21:41:02 -0800

    [feat] add basic styling example

--- src/components/displayNumber.jsx
+++ src/components/displayNumber.jsx
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react';
 
+// require the sass file corresponding to the component
+require('../stylesheets/components/displaynumber.scss');
+
 // This is a stateless function/component. it only takes in a prop and then displays it
 function DisplayNumber({ numToDisplay }) {
   return (
     <div>
-      <h1>{ numToDisplay.get('counter') }</h1>
+      <h3 className="numToDisplay">{ numToDisplay.get('counter') }</h3>
     </div>
   );
 }
