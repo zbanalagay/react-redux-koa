f3602c5a8892ff58945a8b9cf236386ffeb64ba9
Ahmed Modan <ahmed.modan@gmail.com>
2016-03-08 17:41:35 -0800

    [refactor] refactor test

--- __tests__/counterView-test.js
+++ __tests__/counterView-test.js
@@ -1,19 +1,20 @@
 jest.unmock('../src/containers/Counter/counterView');
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import { CounterView } from '../src/containers/Counter/counterView';
 
 describe('CounterView', () => {
-  it('exists', () => {
-    const counterView = TestUtils.isElement(<CounterView />);
-    expect(counterView).toBe(true);
-  });
-  it('should render 3 html elements', () => {
+  function renderCounterView() {
     const renderer = TestUtils.createRenderer();
     renderer.render(<CounterView />);
-    const output = renderer.getRenderOutput();
-    console.log(output);
+    return renderer.getRenderOutput();
+  }
+  it('exists', () => {
+    expect(TestUtils.isElement(<CounterView />)).toBe(true);
+  });
+
+  it('should render 4 elements', () => {
+    const output = renderCounterView();
     expect(output.props.children.length).toBe(4);
   });
 });
